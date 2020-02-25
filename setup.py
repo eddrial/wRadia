718c60d542eac631500a63f96948350489c7f68a
Ed Rial <eddrial@hotmail.com>
2020-02-25 11:32:12 +0100

    radia dependency link included?

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       author = "Ed Rial",
       version = "0.1",
       description='Python wrapper for Radia, easily accessible object attributes',
-      dependency_links=['http://github.com/eddrial/aapy/tarball/master#egg=package-1.0'],
+      dependency_links=['https://github.com/ochubar/Radia/blob/master/cpp/py/setup.py'],
       packages = find_packages(),
       install_requires = ['numpy',
                           'radia'],
