f13efc020bc12ad9efabd96711f24716945be8b3
Ed Rial <eddrial@hotmail.com>
2020-03-02 13:28:46 +0100

    stab in the dark #25

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       author = "Ed Rial",
       version = "0.1",
       description='Python wrapper for Radia, easily accessible object attributes',
-      dependency_links=['https://github.com/ochubar/Radia/blob/master/cpp/py/setup.py'],
+      dependency_links=['https://github.com/ochubar/Radia/tree/master/cpp/py/tarball/master#egg=package-1.0'],
       packages = find_packages(),
       install_requires = ['numpy'],
       )
