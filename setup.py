ab44e4a5278d932b43f588e0edbde6cd9dc26ecf
Ed Rial <eddrial@hotmail.com>
2020-03-03 09:03:00 +0100

    this?

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
       description='Python wrapper for Radia, easily accessible object attributes',
       dependency_links=['http://github.com/ochubar/Radia/tarball/master#egg=package-1.0&subdirectory=cpp/py'],
       packages = find_packages(),
-      install_requires = ['numpy', 
-                          'radia'],
+      install_requires = ['numpy'],
       )
 #
