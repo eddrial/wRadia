c8741a63b86e746ffda04ec674eb17b28ae4a1f5
Ed Rial <eddrial@hotmail.com>
2020-03-03 08:58:05 +0100

    roll back to previous best effort. Problem with reqs to compile, or problem with setup?

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       author = "Ed Rial",
       version = "0.1",
       description='Python wrapper for Radia, easily accessible object attributes',
-      #dependency_links=['http://github.com/ochubar/Radia/tarball/master#egg=package-1.0&subdirectory=cpp/py'],
+      dependency_links=['http://github.com/ochubar/Radia/tarball/master#egg=package-1.0&subdirectory=cpp/py'],
       packages = find_packages(),
       install_requires = ['numpy', 
                           'radia'],
