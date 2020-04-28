4ba06d2c9c63bc901587e189cec77c410282375f
Ed Rial <eddrial@hotmail.com>
2020-04-28 13:11:50 +0200

    Reflection Quaternion implemented (hard code)

--- wRadia/wradObj.py
+++ wRadia/wradObj.py
@@ -75,6 +75,40 @@ class wradObjThckPgn(object):
         #rotate radia object
         rota = rd.TrfRot(pivot_origin,pivot_vector,rot_magnitude)
         rd.TrfOrnt(self.radobj,rota)
+
+    def wradReflect(self,reflection_origin, reflection_vector):
+        '''trying to write a reflection function
+            # u' = quq
+            #u is point
+            #q is quaternion representation of rotation angle ( sin (th/2)i, sin(th/2)j, sin (th/2)k, cos (th/2))'''
+        
+        r = np.linalg.norm(reflection_vector)  #ai + bj + ck
+        tmp = np.zeros(3)
+        #reflect vertices
+        for i in range (len(self.vertices)):
+            u = self.vertices[i] - reflection_origin # xi + yj + zk
+            #i x(-a^2 + b^2 + c^2) -2aby    -2acz
+            #j y(-b^2 + a^2 + c^2)     -2abx    -2bcz
+            #k z(-c^2 + a^2 + b^2)    -2acx    -2bcy
+
+            tmp[0] = u[0] * (-r[0]^2 + r[1]^2 + r[2]^2) -2 * r[0] * (r[1] * u[1] + r[2] * u[2])
+            tmp[1] = u[1] * (-r[1]^2 + r[0]^2 + r[2]^2) -2 * r[1] * (r[0] * u[0] + r[2] * u[2])
+            tmp[2] = u[2] * (-r[2]^2 + r[0]^2 + r[1]^2) -2 * r[2] * (r[0] * u[0] + r[1] * u[1])
+            
+        
+            self.vertices[i] = tmp
+        
+        #reflect magnetisation vector
+        u = self.magnetisation
+        tmp[0] = u[0] * (-r[0]^2 + r[1]^2 + r[2]^2) -2 * r[0] * (r[1] * u[1] + r[2] * u[2])
+        tmp[1] = u[1] * (-r[1]^2 + r[0]^2 + r[2]^2) -2 * r[1] * (r[0] * u[0] + r[2] * u[2])
+        tmp[2] = u[2] * (-r[2]^2 + r[0]^2 + r[1]^2) -2 * r[2] * (r[0] * u[0] + r[1] * u[1])
+        
+        self.magnetisation = tmp
+        
+        #reflect radia object
+        refl = rd.TrfRot(reflection_origin,reflection_vector)
+        rd.TrfOrnt(self.radobj,refl)        
         
     def wradTranslate(self,translation_vector):
         '''trying to write a translation function'''
@@ -82,7 +116,6 @@ class wradObjThckPgn(object):
         #translate vertices
         for i in range (len(self.vertices)):
             self.vertices[i] = self.vertices[i] + translation_vector
-        
         
         #rotate radia object
         tran = rd.TrfTrsl(translation_vector)
@@ -179,7 +212,38 @@ class wradObjCnt(object):
             obj.wradTranslate(translation_vector)#vertices
 
     
-    def wradReflect(self):
+    def wradReflect(self, reflection_origin, reflection_vector):
+        '''trying to write a reflection function'''
+        rd.TrfPlSym()
+        #is this a container, or a primitive... check for object list
+        try:
+            self.objectlist
+        except AttributeError:
+            pass
+
+        #is colour applied at this level?
+        try:
+            self.colour
+            #if yes rotate colour
+            q = R.from_quat([pivot_vector[0] * np.sin(rot_magnitude/2.0), 
+                 pivot_vector[1] * np.sin(rot_magnitude/2.0),
+                 pivot_vector[2] * np.sin(rot_magnitude/2.0),
+                 np.cos(rot_magnitude/2.0),])
+            
+            tmpcol = [(4*x - 2) for x in self.colour]
+            
+            tmpcol = q.apply(tmpcol)
+            
+            self.colour = [(2+x) / 4.0 for x in tmpcol]
+            rd.ObjDrwAtr(self.radobj,self.colour, self.linethickness)
+        except:
+            pass
+        
+        #recur down to overloaded function
+        for obj in self.objectlist:
+            obj.wradReflect(reflection_origin, reflection_vector)
+        
+        
         #vertices
         #magnetisation
         pass
