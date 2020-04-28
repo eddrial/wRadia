57fe2b5cb037753d8e87d3483b5ec4533d8450d4
Ed Rial <eddrial@hotmail.com>
2020-04-28 15:10:21 +0200

    Reflect Now correctly does colours

--- wRadia/wradObj.py
+++ wRadia/wradObj.py
@@ -82,7 +82,7 @@ class wradObjThckPgn(object):
             #u is point
             #q is quaternion representation of rotation angle ( sin (th/2)i, sin(th/2)j, sin (th/2)k, cos (th/2))'''
         
-        r = np.linalg.norm(reflection_vector)  #ai + bj + ck
+        r = reflection_vector / np.linalg.norm(reflection_vector)  #ai + bj + ck
         tmp = np.zeros(3)
         #reflect vertices
         for i in range (len(self.vertices)):
@@ -91,24 +91,24 @@ class wradObjThckPgn(object):
             #j y(-b^2 + a^2 + c^2)     -2abx    -2bcz
             #k z(-c^2 + a^2 + b^2)    -2acx    -2bcy
 
-            tmp[0] = u[0] * (-r[0]^2 + r[1]^2 + r[2]^2) -2 * r[0] * (r[1] * u[1] + r[2] * u[2])
-            tmp[1] = u[1] * (-r[1]^2 + r[0]^2 + r[2]^2) -2 * r[1] * (r[0] * u[0] + r[2] * u[2])
-            tmp[2] = u[2] * (-r[2]^2 + r[0]^2 + r[1]^2) -2 * r[2] * (r[0] * u[0] + r[1] * u[1])
+            tmp[0] = u[0] * (-r[0]**2 + r[1]**2 + r[2]**2) -2 * r[0] * (r[1] * u[1] + r[2] * u[2])
+            tmp[1] = u[1] * (-r[1]**2 + r[0]**2 + r[2]**2) -2 * r[1] * (r[0] * u[0] + r[2] * u[2])
+            tmp[2] = u[2] * (-r[2]**2 + r[0]**2 + r[1]**2) -2 * r[2] * (r[0] * u[0] + r[1] * u[1])
             
         
             self.vertices[i] = tmp
         
         #reflect magnetisation vector
         u = self.magnetisation
-        tmp[0] = u[0] * (-r[0]^2 + r[1]^2 + r[2]^2) -2 * r[0] * (r[1] * u[1] + r[2] * u[2])
-        tmp[1] = u[1] * (-r[1]^2 + r[0]^2 + r[2]^2) -2 * r[1] * (r[0] * u[0] + r[2] * u[2])
-        tmp[2] = u[2] * (-r[2]^2 + r[0]^2 + r[1]^2) -2 * r[2] * (r[0] * u[0] + r[1] * u[1])
+        tmp[0] = u[0] * (-r[0]**2 + r[1]**2 + r[2]**2) -2 * r[0] * (r[1] * u[1] + r[2] * u[2])
+        tmp[1] = u[1] * (-r[1]**2 + r[0]**2 + r[2]**2) -2 * r[1] * (r[0] * u[0] + r[2] * u[2])
+        tmp[2] = u[2] * (-r[2]**2 + r[0]**2 + r[1]**2) -2 * r[2] * (r[0] * u[0] + r[1] * u[1])
         
         self.magnetisation = tmp
         
         #reflect radia object
-        refl = rd.TrfRot(reflection_origin,reflection_vector)
-        rd.TrfOrnt(self.radobj,refl)        
+        refl = rd.TrfPlSym(reflection_origin,reflection_vector)
+        rd.TrfOrnt(self.radobj,refl)
         
     def wradTranslate(self,translation_vector):
         '''trying to write a translation function'''
@@ -214,7 +214,7 @@ class wradObjCnt(object):
     
     def wradReflect(self, reflection_origin, reflection_vector):
         '''trying to write a reflection function'''
-        rd.TrfPlSym()
+        #rd.TrfPlSym()
         #is this a container, or a primitive... check for object list
         try:
             self.objectlist
@@ -224,15 +224,23 @@ class wradObjCnt(object):
         #is colour applied at this level?
         try:
             self.colour
-            #if yes rotate colour
-            q = R.from_quat([pivot_vector[0] * np.sin(rot_magnitude/2.0), 
-                 pivot_vector[1] * np.sin(rot_magnitude/2.0),
-                 pivot_vector[2] * np.sin(rot_magnitude/2.0),
-                 np.cos(rot_magnitude/2.0),])
-            
+            #if yes reflect the colour colour
+            r = reflection_vector/np.linalg.norm(reflection_vector)  #ai + bj + ck
+            tmp = np.zeros(3)
+        #reflect colour
             tmpcol = [(4*x - 2) for x in self.colour]
             
-            tmpcol = q.apply(tmpcol)
+            u = tmpcol # xi + yj + zk
+            #i x(-a^2 + b^2 + c^2) -2aby    -2acz
+            #j y(-b^2 + a^2 + c^2)     -2abx    -2bcz
+            #k z(-c^2 + a^2 + b^2)    -2acx    -2bcy
+
+            tmp[0] = u[0] * (-r[0]**2 + r[1]**2 + r[2]**2) -2 * r[0] * (r[1] * u[1] + r[2] * u[2])
+            tmp[1] = u[1] * (-r[1]**2 + r[0]**2 + r[2]**2) -2 * r[1] * (r[0] * u[0] + r[2] * u[2])
+            tmp[2] = u[2] * (-r[2]**2 + r[0]**2 + r[1]**2) -2 * r[2] * (r[0] * u[0] + r[1] * u[1])
+            
+        
+            tmpcol = tmp
             
             self.colour = [(2+x) / 4.0 for x in tmpcol]
             rd.ObjDrwAtr(self.radobj,self.colour, self.linethickness)
