2d459974e921528e35ea06416f7ae6171f52c0f8
Ed Rial <eddrial@hotmail.com>
2020-07-28 13:50:23 +0200

    Extended test suite for wradobjthckpgn attributes

--- test/test_wradObj.py
+++ test/test_wradObj.py
@@ -13,13 +13,72 @@ import numpy as np
 class Test(unittest.TestCase):
     #parameters
     
-    def test_wradObjThckPgn_exists(self,mainmagthick = 5, origin = [0,0,0]):
-        a = rd.ObjThckPgn(0, 5, [[-5,-5],[-5,5],[5,5],[5,-5]],'x',origin)
-        assert a == 1
+    ###wradObjThckPgn __init__ tests
+    def test_wradObjThckPgn_exists(self):
+        rd.UtiDelAll()
+        a = wrd.wradObjThckPgn(0, 5, [[0,10],[10,10],[10,0],[0,0]],'x',[0,0,0])
+        #rd.ObjThckPgn(0, 5, [[0,10],[0,10],[10,10],[0,0]],'x',origin)
+        assert a.radobj == 1
         
         #assert wrd.wradObjThckPgn(0, mainmagthick, ([-5,-5],[-5,5],[5,5],[5,-5]),'x',origin).radobj == 1
 
+    def test_wradObjThckPgn_corners(self):
+        rd.UtiDelAll()
+        a = wrd.wradObjThckPgn(0, 5, [[0,10],[10,10],[10,0],[0,0]],'x',[0,0,0])
+        
+        assert a.corners == [[0,10],[10,10],[10,0],[0,0]]
+        
+    def test_wradObjThckPgn_extrusion_direction(self):
+        rd.UtiDelAll()
+        a = wrd.wradObjThckPgn(0, 5, [[0,10],[10,10],[10,0],[0,0]],'x',[0,0,0])
+        
+        assert a.extrusion_direction == 'x'
+        
+    def test_wradObjThckPgn_thickness(self):
+        rd.UtiDelAll()
+        a = wrd.wradObjThckPgn(0, 5, [[0,10],[10,10],[10,0],[0,0]],'x',[0,0,0])
+        
+        assert a.lx == 5
+        
+    def test_wradObjThckPgn_vertices_x_extrusion(self):
+        rd.UtiDelAll()
+        a = wrd.wradObjThckPgn(0, 5, [[0,10],[10,10],[10,0],[0,0]],'x',[0,0,0])
+        
+        comparison = a.vertices == np.array([[-2.5,  0. , 10. ],[-2.5, 10. , 10. ],[-2.5, 10. ,  0. ],[-2.5,  0. ,  0. ],[ 2.5,  0. , 10. ],[ 2.5, 10. , 10. ],[ 2.5, 10. ,  0. ],[ 2.5,  0. ,  0. ]])
 
+        assert comparison.all()
+        
+    def test_wradObjThckPgn_vertices_y_extrusion(self):
+        rd.UtiDelAll()
+        a = wrd.wradObjThckPgn(0, 5, [[0,10],[10,10],[10,0],[0,0]],'y',[0,0,0])
+        
+        comparison = a.vertices == np.array([[10. , -2.5,  0. ], [10. , -2.5, 10. ], [ 0. , -2.5, 10. ], [ 0. , -2.5,  0. ], [10. ,  2.5,  0. ], [10. ,  2.5, 10. ], [ 0. ,  2.5, 10. ], [ 0. ,  2.5,  0. ]])
+
+        assert comparison.all()
+        
+    def test_wradObjThckPgn_vertices_z_extrusion(self):
+        rd.UtiDelAll()
+        a = wrd.wradObjThckPgn(0, 5, [[0,10],[10,10],[10,0],[0,0]],'z',[0,0,0])
+        
+        comparison = a.vertices == np.array([[ 0. , 10. , -2.5], [10. , 10. , -2.5], [10. ,  0. , -2.5], [ 0. ,  0. , -2.5], [ 0. , 10. ,  2.5], [10. , 10. ,  2.5], [10. ,  0. ,  2.5], [ 0. ,  0. ,  2.5]])
+
+        assert comparison.all()
+        
+    def test_wradObjThckPgn_magnetisation(self):
+        rd.UtiDelAll()
+        a = wrd.wradObjThckPgn(0, 5, [[0,10],[10,10],[10,0],[0,0]],'x',[1,2,3])
+        
+        assert a.magnetisation == [1,2,3]
+        
+    def test_wradObjThckPgn_extrusion_centre_of_mass(self):
+        rd.UtiDelAll()
+        a = wrd.wradObjThckPgn(0, 5, [[0,10],[10,10],[10,0],[0,0]],'x',[1,2,3])
+        
+        assert a.x == 0
+        
+    #
+        
+    
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test_wradObjThckPgn_exists']
     unittest.main()
