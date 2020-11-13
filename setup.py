fb7b11ebb71208369781e3e4a1e909530c7cda55
Ed Rial <eddrial@hotmail.com>
2020-11-13 15:12:02 +0100

    yaml and setup compatibilities

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
       version = "0.1",
       description='Python wrapper for Radia, easily accessible object attributes',
       dependency_links=['http://github.com/ochubar/Radia/tarball/master#egg=package-1.0&subdirectory=env/radia_python'],
-      packages = find_packages(),
-      install_requires = ['numpy',
-                          'radia',
-                          ''],
-      package_data={'': ['radia_py3_7_x86_64.so']},
+      packages = find_packages()#,
+#      install_requires = ['numpy',
+#                          'radia',
+#                          ''],
+#      package_data={'': ['radia_py3_7_x86_64.so']},
       )
 #
