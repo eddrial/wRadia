33a23606da0cbf615c9835e8aa1aa2cbd36c8104
Ed Rial <eddrial@hotmail.com>
2021-11-09 10:43:32 +0100

    redundant try catches removed from wradObjThckPgn

--- wradia/wrad_obj.py
+++ wradia/wrad_obj.py
@@ -100,23 +100,20 @@ class wradObjThckPgn(object):
         self.magnetisation = (q.apply(self.magnetisation)).tolist()
         self.material.M = self.magnetisation
         
-        #is colour applied at this level?
-        try:
-            self.colour
-            #if yes rotate colour
-            q = R.from_quat([pivot_vector[0] * np.sin(rot_magnitude/2.0), 
-                 pivot_vector[1] * np.sin(rot_magnitude/2.0),
-                 pivot_vector[2] * np.sin(rot_magnitude/2.0),
-                 np.cos(rot_magnitude/2.0),])
-            
-            tmpcol = [(4*x - 2) for x in self.colour]
-            
-            tmpcol = q.apply(tmpcol)
-            
-            self.colour = [(2+x) / 4.0 for x in tmpcol]
-            rd.ObjDrwAtr(self.radobj,self.colour, self.linethickness)
-        except:
-            pass
+        
+        # rotate colour
+        q = R.from_quat([pivot_vector[0] * np.sin(rot_magnitude/2.0), 
+             pivot_vector[1] * np.sin(rot_magnitude/2.0),
+             pivot_vector[2] * np.sin(rot_magnitude/2.0),
+             np.cos(rot_magnitude/2.0),])
+        
+        tmpcol = [(4*x - 2) for x in self.colour]
+        
+        tmpcol = q.apply(tmpcol)
+        
+        self.colour = [(2+x) / 4.0 for x in tmpcol]
+        rd.ObjDrwAtr(self.radobj,self.colour, self.linethickness)
+        
         
         #rotate radia object
         rota = rd.TrfRot(pivot_origin,pivot_vector,rot_magnitude)
@@ -154,30 +151,28 @@ class wradObjThckPgn(object):
         self.material.M = self.magnetisation
         
         #is colour applied at this level?
-        try:
-            self.colour
-            #if yes reflect the colour colour
-            r = reflection_vector/np.linalg.norm(reflection_vector)  #ai + bj + ck
-            tmp = np.zeros(3)
-        #reflect colour
-            tmpcol = [(4*x - 2) for x in self.colour]
-            
-            u = tmpcol # xi + yj + zk
-            #i x(-a^2 + b^2 + c^2) -2aby    -2acz
-            #j y(-b^2 + a^2 + c^2)     -2abx    -2bcz
-            #k z(-c^2 + a^2 + b^2)    -2acx    -2bcy
-
-            tmp[0] = u[0] * (-r[0]**2 + r[1]**2 + r[2]**2) -2 * r[0] * (r[1] * u[1] + r[2] * u[2])
-            tmp[1] = u[1] * (-r[1]**2 + r[0]**2 + r[2]**2) -2 * r[1] * (r[0] * u[0] + r[2] * u[2])
-            tmp[2] = u[2] * (-r[2]**2 + r[0]**2 + r[1]**2) -2 * r[2] * (r[0] * u[0] + r[1] * u[1])
-            
         
-            tmpcol = tmp
-            
-            self.colour = [(2+x) / 4.0 for x in tmpcol]
-            rd.ObjDrwAtr(self.radobj,self.colour, self.linethickness)
-        except:
-            pass
+        #reflect the colour
+        r = reflection_vector/np.linalg.norm(reflection_vector)  #ai + bj + ck
+        tmp = np.zeros(3)
+    #reflect colour
+        tmpcol = [(4*x - 2) for x in self.colour]
+        
+        u = tmpcol # xi + yj + zk
+        #i x(-a^2 + b^2 + c^2) -2aby    -2acz
+        #j y(-b^2 + a^2 + c^2)     -2abx    -2bcz
+        #k z(-c^2 + a^2 + b^2)    -2acx    -2bcy
+
+        tmp[0] = u[0] * (-r[0]**2 + r[1]**2 + r[2]**2) -2 * r[0] * (r[1] * u[1] + r[2] * u[2])
+        tmp[1] = u[1] * (-r[1]**2 + r[0]**2 + r[2]**2) -2 * r[1] * (r[0] * u[0] + r[2] * u[2])
+        tmp[2] = u[2] * (-r[2]**2 + r[0]**2 + r[1]**2) -2 * r[2] * (r[0] * u[0] + r[1] * u[1])
+        
+    
+        tmpcol = tmp
+        
+        self.colour = [(2+x) / 4.0 for x in tmpcol]
+        rd.ObjDrwAtr(self.radobj,self.colour, self.linethickness)
+        
         
         #reflect radia object
         refl = rd.TrfPlSym(reflection_origin,reflection_vector)
@@ -205,21 +200,19 @@ class wradObjThckPgn(object):
         fieldinvert = rd.TrfInv(self.radobj)
         rd.TrfOrnt(self.radobj,fieldinvert)    
 
-        try:
-            self.colour
-            #if yes invert the colour colour
-            tmp = np.zeros(3)
-        #reflect colour
-            tmpcol = [(4*x - 2) for x in self.colour]
-            
-            tmpcol[0] = -tmpcol[0]
-            tmpcol[1] = -tmpcol[1]
-            tmpcol[2] = -tmpcol[2]
-            
-            self.colour = [(2+x) / 4.0 for x in tmpcol]
-            rd.ObjDrwAtr(self.radobj,self.colour, self.linethickness)
-        except:
-            pass
+        
+        #invert the colour
+        tmp = np.zeros(3)
+    #reflect colour
+        tmpcol = [(4*x - 2) for x in self.colour]
+        
+        tmpcol[0] = -tmpcol[0]
+        tmpcol[1] = -tmpcol[1]
+        tmpcol[2] = -tmpcol[2]
+        
+        self.colour = [(2+x) / 4.0 for x in tmpcol]
+        rd.ObjDrwAtr(self.radobj,self.colour, self.linethickness)
+        
         
 #        self.radobj.TrfInv()
         
