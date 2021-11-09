4c84f44d73b1495c4024e8c888cfb4e3b320c6ab
Ed Rial <eddrial@hotmail.com>
2021-11-09 21:35:15 +0100

    test container matappl

--- test/test_wrad_obj.py
+++ test/test_wrad_obj.py
@@ -9,6 +9,8 @@ import wradia as wrd
 import numpy as np
 import copy
 from _pytest.outcomes import fail
+from unittest.mock import patch
+from wradia.wrad_obj import wradObjCnt, wradObjThckPgn
 
 
 
@@ -1078,8 +1080,50 @@ class Test_wradFieldInvert_container(unittest.TestCase):
         self.assertLess(b[2], -0.01, 'field did not invert')
       
       
-
-
+class Test_wradMatAppl_container(unittest.TestCase):
+    #wradMatAppl. Testing application of material parameters down into containers
+    def setUp(self):
+        
+        #Test suite to see if reflecting a container of two thick polygons correctly reflects magnetisation vector, colour, and calls the correct Radia function
+        # Need 4 blocks 
+        
+        rd.UtiDelAll()
+        self.ksi = [.019, .06]
+        
+        #defince an array of magnetisatins [x+, x-, y+, y-, z+, z-]
+        magnetisations = [[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]]
+        
+        #define array of materials to apply to the blocks
+        self.materials = [[] for _ in range(6)]
+        for i in range(6):
+            self.materials[i] = wrd.wrad_mat.wradMatLin(self.ksi, magnetisations[i])
+        #create an array of magnet blocks for manipulation
+        self.test_blocks = [[] for _ in range(6)]
+        for i in range(6):
+            self.test_blocks[i] = wrd.wrad_obj.wradObjThckPgn(0, 10, [[-5,5],[5,5],[5,-5],[-5,-5]],'x',[0,0,0])
+            self.test_blocks[i].wradMatAppl(self.materials[i]) 
+            self.test_blocks[i].wradObjDrwAtr(colour = 'default', linethickness = 2)
+        
+        #create an array of magnet blocks as comparators
+        self.comparator_blocks = [[] for _ in range(6)]
+        for i in range(6):
+            self.comparator_blocks[i] = wrd.wrad_obj.wradObjThckPgn(0, 10, [[-5,5],[5,5],[5,-5],[-5,-5]],'x',[0,0,0])
+            self.comparator_blocks[i].wradMatAppl(self.materials[i]) 
+            self.comparator_blocks[i].wradObjDrwAtr(colour = 'default', linethickness = 2)
+        
+        #create containers for X, Y Z blocks
+        self.test_containers = [[] for _ in range(3)]
+        for i in range(3):
+            self.test_containers[i] = wrd.wrad_obj.wradObjCnt([self.test_blocks[2*i],self.test_blocks[2*i + 1]])
+            #self.test_containers[i] = wrd.wrad_obj.wradObjCnt([self.test_blocks[2*i]])
+    
+    
+    @unittest.mock.patch.object(wradObjThckPgn,"wradMatAppl")
+    def test_contMatAppl(self,mock):
+        self.test_containers[0].wradMatAppl(self.materials[0])
+        #mock.assert_called()    
+        np.testing.assert_equal(mock.call_count,2)
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test_wradObjThckPgn_exists']
     unittest.main()
